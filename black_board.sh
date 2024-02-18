ed78f93f0e705823b6a76918705784dfa25eab9a
morehao <morehao@qq.com>
2024-02-18 17:52:06 +0800

    feat(feature): 数据类型

---
+++ black_board.sh
@@ -0,0 +1,5 @@
+str="abc"
+echo expr index $str "a" # 1
+echo expr index $str "b" # 2
+echo expr index $str "x" # 0
+echo expr index $str "" # 0
---
+++ shellFeature/typeString/declare.sh
@@ -0,0 +1,5 @@
+name="小明"
+
+full_name="王${name}"
+
+echo $full_name
